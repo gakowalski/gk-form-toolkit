a91ab265b6c0aa27d6332282fb4a7b7500b911a6
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2022-05-10 00:42:40 +0200

    Wprowadzenie dyrektywy `html` Blade

--- src/Laravel/ServiceProvider.php
+++ src/Laravel/ServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace Kowalski\Laravel;
 
+use Illuminate\Support\Facades\Blade;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot() {
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        
+        Blade::directive('html', function ($expression) {
+          return "<?php echo \App\Html::$expression ?>";
+        });
     }
 
     public function register() {
